db5bf39e5bba3689d10970049105ec569fa2aae8
Andrej Oljaca <65299043+aoljaca@users.noreply.github.com>
2021-05-21 11:52:24 -0400

    commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
     return (
         <div className="hero-container">
             <video autoPlay muted loop controls>
-                <source src="https://vod-progressive.akamaized.net/exp=1621606007~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2775%2F21%2F538877060%2F2554448071.mp4~hmac=1df0c0890554a24b62c5fc65e76f0d0fbcc8d4ed2e3be9a7381aedf97249e6b7/vimeo-prod-skyfire-std-us/01/2775/21/538877060/2554448071.mp4?filename=Waves+-+70796.mp4" type="video/mp4" />
+                <source src="./videos/video-1.mp4" type="video/mp4" />
             </video>
             <h1>Adventure Awaits</h1>
             <p>What are you waiting for</p>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import HeroSection from '../HeroSection';
 
 import React from 'react'
 import Cards from '../Cards.js';
+import Footer from '../Footer';
 
 function Home() {
     return (
         <div>
             <HeroSection />
             <Cards />
+            <Footer />
         </div>
     )
 }
